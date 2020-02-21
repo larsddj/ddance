4365e62eb3a47998992cae7bb66b388c577ee7c4
Lars <lars.d.j@hotmail.com>
2020-02-21 01:39:53 +0100

    hardcoded color of lifemeter orange

--- BGAnimations/ScreenGameplay underlay/PerPlayer/LifeMeter/Vertical.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/LifeMeter/Vertical.lua
@@ -41,7 +41,7 @@ local meter = Def.ActorFrame{
 	-- // start meter proper //
 	Def.Quad{
 		Name="MeterFill";
-		InitCommand=function(self) self:zoomto(width,0):diffuse(color("#7623ba")):align(0,1) end,
+		InitCommand=function(self) self:zoomto(width,0):diffuse(color("#dd6b00")):align(0,1) end,
 		OnCommand=function(self) self:xy( _x - width/2, height/2) end,
 
 		-- check life (LifeMeterBar)
